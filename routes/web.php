b418b39b8c6117269faebed75b07538c1eade2eb
serii burduja <seriiburduja@gmail.com>
2024-11-10 19:58:33 +0200

    feat: get controller in api

--- routes/web.php
+++ routes/web.php
@@ -12,11 +12,11 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::get('/{any}', function () {
     return view('welcome');
 })->where('any', '.*');
 
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
