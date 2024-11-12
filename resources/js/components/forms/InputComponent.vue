f6ceecc4bb94430bc307d47ff2343ab9202c1522
serii burduja <seriiburduja@gmail.com>
2024-11-12 16:51:53 +0200

    feat: input component

---
+++ resources/js/components/forms/InputComponent.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+const emits = defineEmits(["update:value"]);
+
+const pros = defineProps({
+    name: String,
+    placeholder: String,
+    value: String,
+});
+
+function changeHandler(e: any) {
+    if (!e.target) return;
+    let value = e.target.value;
+    emits("update:value", value);
+}
+</script>
+
+<template>
+    <input
+        :name="name"
+        class="form-control"
+        :placeholder="placeholder"
+        :value="value"
+        @input="changeHandler"
+    />
+</template>
--- resources/js/views/Personal.vue
+++ resources/js/views/Personal.vue
@@ -1,7 +1,33 @@
 <script setup>
-import axios from "axios";
+import { ref } from "vue";
+import InputComponent from "../components/forms/InputComponent.vue";
+
+const title = ref("");
+const content = ref("");
 </script>
 
 <template>
-    <div>Personal</div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="mb-3">
+                    <InputComponent
+                        name="title"
+                        placeholder="Title"
+                        v-model:value="title"
+                    />
+                </div>
+                <div class="mb-3">
+                    <textarea
+                        name="content"
+                        class="form-control"
+                        v-model="content"
+                        placeholder="content"
+                        rows="10"
+                    ></textarea>
+                </div>
+                <button type="btn" class="btn btn-primary">Submit</button>
+            </div>
+        </div>
+    </div>
 </template>
