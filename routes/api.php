2cb3a4d02270d78f854d6a51cd8ac049c6657893
serii burduja <seriiburduja@gmail.com>
2024-11-12 16:58:51 +0200

    feat: post ressource controller

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,4 +21,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::group(['middleware' => 'auth:sanctum'], function () {
     Route::get('/get', [App\Http\Controllers\GetController::class, 'index']);
+    Route::resource('posts', PostController::class);
 });
