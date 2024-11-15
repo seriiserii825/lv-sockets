5548b980ba475001db61654ddd00cbdab2f98671
serii burduja <seriiburduja@gmail.com>
2024-11-15 16:22:39 +0200

    feat: posts table added repost_id

---
+++ database/migrations/2024_11_15_142054_add_reposted_id_to_posts_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->foreignId('reposted_id')->nullable()->constrained('posts')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->dropConstrainedForeignId('reposted_id');
+        });
+    }
+};
